a61e9ae270cfe285d0daa68f688d98987135e15e
Esancal <esteban.ca2000@outlook.cl>
2022-05-24 10:02:43 -0400

    subiendo

---
+++ IntelliJ/Ejemplo1/src/cl/generation/nombres/Esteban/Main.java
@@ -0,0 +1,23 @@
+package cl.generation.nombres.Esteban;
+/**  Esto es para documentacion
+ *   Metodo o funcion Main
+ **/
+
+public class Main {
+    //Esto es un comentario
+    /*
+    Varias
+    Lineas
+    de
+    comentario
+     */
+    public static void main(String[] args) {
+
+    for(int a=0; a<10; a++){
+        System.out.print("El valor de a es: ");
+        System.out.println(a);
+    }
+}
+
+}
+
