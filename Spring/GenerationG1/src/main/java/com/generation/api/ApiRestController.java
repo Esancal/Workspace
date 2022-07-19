9a6d7ded8174770c1f1a48954a246f62d6637c45
Esancal <esteban.ca2000@outlook.cl>
2022-07-19 12:08:08 -0400

    enviar json

--- Spring/GenerationG1/src/main/java/com/generation/api/ApiRestController.java
+++ Spring/GenerationG1/src/main/java/com/generation/api/ApiRestController.java
@@ -3,7 +3,11 @@ package com.generation.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +26,13 @@ public class ApiRestController {
     public List<Auto> obtenerListaAutos() {
         List<Auto> listaAutos = autoService.findAll();
         return listaAutos;
+    }
+
+    // para recibir json
+    @PostMapping(value = "/guardar/auto")
+    public ResponseEntity<Auto> guardarAuto(@RequestBody Auto auto){
+        autoService.saveAuto(auto);
+        return new ResponseEntity<Auto>(auto, HttpStatus.OK);
     }
 
     // API (JSON) JavaScript Notacion
